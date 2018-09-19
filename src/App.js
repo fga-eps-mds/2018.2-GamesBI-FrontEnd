1bdd903ee86ef6ab637a5f5e4d1790030bd69030
Alan Lima <alanronison.lima@gmail.com>
2018-09-18 21:18:45 -0300

    [UPDATE] Default layout applied to frontEnd

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ import 'simple-line-icons/css/simple-line-icons.css';
 import './scss/style.css'
 
 // Containers
-import { HomeLayout } from './containers';
+import { DefaultLayout } from './containers';
 // Pages
 import { Login, Page404, Page500, Register } from './views/Pages';
 
@@ -29,7 +29,7 @@ class App extends Component {
           <Route exact path="/register" name="Register Page" component={Register} />
           <Route exact path="/404" name="Page 404" component={Page404} />
           <Route exact path="/500" name="Page 500" component={Page500} />
-          <Route path="/" name="Home" component={HomeLayout} />
+          <Route path="/" name="Home" component={DefaultLayout} />
         </Switch>
       </HashRouter>
     );
